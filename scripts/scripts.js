13e215c41dfac9e6f2fab7885aba23595904ab83
Alan Huang <alan.huang.2@stonybrook.edu>
2022-09-24 22:22:54 -0400

    .

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,34 +6,81 @@ const bImage = document.querySelector(".b-image");
 const parkList = document.querySelector(".park-list");
 
 const bImages = ["brooklyn", "bronx", "manhattan", "queens", "staten_island"];
-const parks = [["Prospect Park", "Walk Whitman Park", "Marine Park", "Fort Greene Park", "Canarsie Park", "Sunset Park", "Kaiser Park"], ["Crotona Park", "Franz Sigel Park", "Ferry Point Park", "Mullaly Park", "Soundview Park", "Mill Pond Park"], ["Central Park", "Washington Square Park", "Battery Park", "Bryant Park", "Isham Park"], ["Kissena Park", "Forest Park", "Elmhust Park", "Travers Park", "Astoria Park", "Bowne Park"], ["High Rock Park", "Silver Lake Park", "Brookfield Park", "Ocean Breeze Park", "Tappen Park", "Willowbrook Park"]];
+const parks = [
+    [
+        "Prospect Park",
+        "Walk Whitman Park",
+        "Marine Park",
+        "Fort Greene Park",
+        "Canarsie Park",
+        "Sunset Park",
+        "Kaiser Park",
+    ],
+    [
+        "Crotona Park",
+        "Franz Sigel Park",
+        "Ferry Point Park",
+        "Mullaly Park",
+        "Soundview Park",
+        "Mill Pond Park",
+    ],
+    [
+        "Central Park",
+        "Washington Square Park",
+        "Battery Park",
+        "Bryant Park",
+        "Isham Park",
+    ],
+    [
+        "Kissena Park",
+        "Forest Park",
+        "Elmhust Park",
+        "Travers Park",
+        "Astoria Park",
+        "Bowne Park",
+    ],
+    [
+        "High Rock Park",
+        "Silver Lake Park",
+        "Brookfield Park",
+        "Ocean Breeze Park",
+        "Tappen Park",
+        "Willowbrook Park",
+    ],
+];
 
 logo.onclick = () => {
-  mapContent.style.display = "flex";
-  boroughContent.style.display = "none";
-}
+    mapContent.style.display = "flex";
+    boroughContent.style.display = "none";
+};
 
 buttons.forEach((button, index) => {
-  button.onclick = (event) => {
-    event.preventDefault();
-    mapContent.style.display = "none";
-    boroughContent.style.display = "flex";
-    bImage.src = `../images/${bImages[index % 5]}.png`;
-    populateList(index % 5);
-  }
+    button.onclick = (event) => {
+        event.preventDefault();
+        mapContent.style.display = "none";
+        boroughContent.style.display = "flex";
+        bImage.src = `../images/${bImages[index % 5]}.png`;
+        populateList(index % 5);
+    };
 });
 
 function populateList(index) {
-  parkList.innerHTML = "";
+    parkList.innerHTML = "";
 
-  for (let i = 0; i < parks[index].length; i++) {
-    let parkContainer = document.createElement("div");
-    let listItem = document.createElement("p");
-    let parkImg = document.createElement("img");
-    parkImg.src = "../images/park_holder.webp";
-    listItem.innerHTML = parks[index][i];
-    parkContainer.appendChild(parkImg);
-    parkContainer.appendChild(listItem);
-    parkList.appendChild(parkContainer);
-  }
+    for (let i = 0; i < parks[index].length; i++) {
+        let parkContainer = document.createElement("div");
+        let listItem = document.createElement("p");
+
+        let parkImg = document.createElement("img");
+        parkImg.src = "../images/park_holder.webp";
+        listItem.innerHTML = parks[index][i];
+        parkContainer.setAttribute(
+            "onclick",
+            `selectPark("${parks[index][i]}")`
+        );
+
+        parkContainer.appendChild(parkImg);
+        parkContainer.appendChild(listItem);
+        parkList.appendChild(parkContainer);
+    }
 }
