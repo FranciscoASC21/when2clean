765afed15763d4fc92c497d767f3febc1384b3d5
cindy-code <cindy.chen.5@stonybrook.edu>
2022-09-25 02:59:31 -0400

    finished borough markers

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,8 +7,8 @@ const bMap = document.querySelector(".b-map");
 const parkList = document.querySelector(".park-list");
 
 const bImages = ["brooklyn", "bronx", "manhattan", "queens", "staten_island"];
-const parks = [["Prospect Park", "Fort Greene Park", "Canarsie Park", "Sunset Park", "Kaiser Park"], ["Bronx Park", "Crotona Park", "Van Cortlandt Park", "Soundview Park", "Claremont Park"], ["Central Park", "Marcus Garvey Park", "Morningside Park"], ["Kissena Park", "Forest Park", "Astoria Park", "Alley Pond Park", "Hook Creek Park"], ["Brookfield Park", "Bloomingdale Park", "Great Kills Park", "Clove Lakes Park"]];
-const parkCoords = [[[-40, 27], [-42, 14.5], [-17.5, 41.5], [-51, 32.5], [-47.5, 61]], [[-40, 32], [-49, 40], [-44, 10], [-39, 52], [-53.5, 38]], [[-23, 35], [-16, 23.7], [-20.5, 23.7]], [[-35, 17], [-46, 33], [-68, 3.5], [-18, 18], [-18, 54]], [[-43, 32], [-60, 48], [-30, 40], [-25, 9]]]
+const parks = [["Prospect Park", "Fort Greene Park", "Canarsie Park", "Sunset Park", "Kaiser Park"], ["Bronx Park", "Crotona Park", "Van Cortlandt Park", "Soundview Park", "Claremont Park"], ["Central Park", "Marcus Garvey Park", "Inwood Hill Park"], ["Kissena Park", "Forest Park", "Astoria Park", "Alley Pond Park", "Hook Creek Park"], ["Brookfield Park", "Bloomingdale Park", "Great Kills Park", "Clove Lakes Park"]];
+const parkCoords = [[[-40, 27], [-42, 14.5], [-17.5, 41.5], [-51, 32.5], [-47.5, 61]], [[-40, 32], [-49, 40], [-44, 10], [-39, 52], [-53.5, 38]], [[-23, 35], [-16, 23.7], [-10.5, -1.5]], [[-35, 17], [-46, 33], [-68, 3.5], [-18, 18], [-18, 54]], [[-43, 32], [-60, 48], [-30, 40], [-25, 9]]]
 
 logo.onclick = () => {
   mapContent.style.display = "flex";
@@ -40,11 +40,19 @@ function createMarkers(index) {
     bMap.appendChild(marker);
 
     marker.onmouseover = function () {
+      marker.style.transform = `translate(${parkCoords[index][i][0]-1.5}vh, ${parkCoords[index][i][1]-2}vh)`;
+      marker.style.width = "11.9vh";
       marker.style.filter = "invert(88%) sepia(24%) saturate(1000%) hue-rotate(287deg) brightness(60%) contrast(106%)";
     };
 
     marker.onmouseout = function () {
+      marker.style.transform = `translate(${parkCoords[index][i][0]}vh, ${parkCoords[index][i][1]}vh)`;
+      marker.style.width = "8.5vh";
       marker.style.filter = "invert(88%) sepia(24%) saturate(1000%) hue-rotate(287deg) brightness(99%) contrast(106%)";
+    };
+
+    marker.onclick = () => {
+      window.location.href = `../main/grass.html?name=${parks[index][i]}`;
     };
   }
 };
@@ -72,7 +80,7 @@ function populateList(index) {
 
     parkContainer.onmouseover = function () {
       parkContainer.style.backgroundColor = "#fcd6d6"
-      listItem.style.color = "hotpink";
+      listItem.style.color = "#c12664";
       listItem.style.fontWeight = "bold";
       parkContainer.style.cursor = "pointer";
     };
