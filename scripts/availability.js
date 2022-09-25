40e346b9fe7adb5e49fd76b1742052bb49401689
FranciscoASC21 <kyriacoudf@gmail.com>
2022-09-25 02:02:11 -0400

    Added async request of data to fill in slider.

--- scripts/availability.js
+++ scripts/availability.js
@@ -1,6 +1,20 @@
 let database = firebase.database().ref();
-
+let currentAvailability = document.querySelector(".current-av");
 const button = document.querySelector(".submitButton");
+const dateDiv = document.querySelector(".date_div");
+let response;
+
+async function getData() {
+  await database.on("value", (dataResponse) => {
+    response = dataResponse.val();
+    updateAval();
+    dateDiv.onmouseover = function () {
+      updateAval();
+    }
+  });
+}
+
+getData();
 
 var url = document.location.href,
   params = (url.split("?")[1] ?? "").split("&"),
@@ -9,6 +23,26 @@ var url = document.location.href,
 for (var i = 0, l = params.length; i < l; i++) {
   tmp = params[i].split("=");
   data[tmp[0]] = tmp[1];
+}
+
+function updateAval() {
+  let colorValues = [];
+  let park = decodeURI(data.name);
+  if (park) {
+    for (element in response) {
+      if (response[element].PARK == park
+        && document.getElementsByClassName("date_div")[0].value == response[element].DATE) {
+        colorValues.push(response[element].SEQUENCE.slice(0, 12));
+      }
+    }
+  }
+
+  let ops = colorValues.length == 0 ? [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] : getOpacities(colorValues);
+  let avalChildren = currentAvailability.children;
+
+  for (let i = 0; i < avalChildren.length; i++) {
+    avalChildren.item(i).style.backgroundColor = `rgba(0, 255, 0, ${ops[i]})`;
+  }
 }
 
 const bars = document.querySelectorAll("li");
