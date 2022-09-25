d8105113cc35d0c9ee78111f6ce839c8360236bb
leylatuon <leylatuon@users.noreply.github.com>
2022-09-24 23:38:11 -0400

    remove more comments

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -86,10 +86,3 @@ function populateList(index) {
     parkList.appendChild(parkContainer);
   }
 }
-
-// function testJS() {
-//     var b = document.getElementById('name').value,
-//         url = 'http://path_to_your_html_files/next.html?name=' + encodeURIComponent(b);
-
-//     document.location.href = url;
-// }
